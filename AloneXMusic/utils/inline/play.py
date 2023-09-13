f29e495f92755ba8c4e46eef3836bbda5aacc161
ZaraPvtBot <142078798+AlonePvtGithub@users.noreply.github.com>
2023-09-14 02:02:08 +0530

    Update play.py

--- AloneXMusic/utils/inline/play.py
+++ AloneXMusic/utils/inline/play.py
@@ -33,25 +33,25 @@ def stream_markup_timer(_, chat_id, played, dur):
     percentage = (played_sec / duration_sec) * 100
     umm = math.floor(percentage)
     if 0 < umm <= 10:
-        bar = "♡═════════"
+        bar = "⚪─────────"
     elif 10 < umm < 20:
-        bar = "═♡════════"
+        bar = "━⚪────────"
     elif 20 <= umm < 30:
-        bar = "══♡═══════"
+        bar = "━━⚪───────"
     elif 30 <= umm < 40:
-        bar = "═══♡══════"
+        bar = "━━━⚪──────"
     elif 40 <= umm < 50:
-        bar = "════♡═════"
+        bar = "━━━━⚪─────"
     elif 50 <= umm < 60:
-        bar = "═════♡════"
+        bar = "━━━━━⚪────"
     elif 60 <= umm < 70:
-        bar = "══════♡═══"
+        bar = "━━━━━━⚪───"
     elif 70 <= umm < 80:
-        bar = "═══════♡══"
+        bar = "━━━━━━━⚪──"
     elif 80 <= umm < 95:
-        bar = "════════♡═"
+        bar = "━━━━━━━━⚪─"
     else:
-        bar = "═════════♡"
+        bar = "━━━━━━━━━⚪"
     buttons = [
         [
             InlineKeyboardButton(
@@ -66,6 +66,15 @@ def stream_markup_timer(_, chat_id, played, dur):
             InlineKeyboardButton(text="‣‣I", callback_data=f"ADMIN Skip|{chat_id}"),
             InlineKeyboardButton(text="▢", callback_data=f"ADMIN Stop|{chat_id}"),
         ],
+        [
+            InlineKeyboardButton(
+                text="[🇮🇳] Aʟᴏɴᴇ", url=f"https://t.me/ALONE_WAS_BOT"
+            ),
+            InlineKeyboardButton(
+                text="Cʜᴀᴛ Gʀᴏᴜᴘ", url=f"https://t.me/AlonesHeaven"
+            ),
+        ],
+        [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
     ]
     return buttons
 
