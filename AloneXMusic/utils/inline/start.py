d90b430c169115c726359cba852267138729754d
ZaraPvtBot <142078798+AlonePvtGithub@users.noreply.github.com>
2023-09-16 14:10:49 +0530

    Update start.py

--- AloneXMusic/utils/inline/start.py
+++ AloneXMusic/utils/inline/start.py
@@ -32,5 +32,7 @@ def private_panel(_):
         [
             InlineKeyboardButton(text=_["S_B_5"], user_id=config.OWNER_ID),
         ],
+        [
+            InlineKeyboardButton(text=_["ST_B_3"], callback_data="LG")]
     ]
     return buttons
