2d56b6aa5c0b0a4e0e2be33136880b91bdf78b27
ZaraPvtBot <142078798+AlonePvtGithub@users.noreply.github.com>
2023-09-16 14:18:49 +0530

    Update start.py

--- AloneXMusic/utils/inline/start.py
+++ AloneXMusic/utils/inline/start.py
@@ -31,7 +31,7 @@ def private_panel(_):
         ],
         [
             InlineKeyboardButton(text=_["S_B_6"], url=config.SUPPORT_CHANNEL),
-            InlineKeyboardButton(text=_["S_B_7"], callback_data="gib_source)],
+            InlineKeyboardButton(text=_["S_B_7"], callback_data="gib_source")],
         ],
         [
             InlineKeyboardButton(text=_["ST_B_3"], callback_data="LG")]
